1ca5e33457a7f611745312d7631a6d6947fb8865
鼠chipmunk <132239368+gydgzh@users.noreply.github.com>
2026-03-05 15:50:59 +0000

    Add build: Intermediates.noindex

---
+++ app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/Objects-normal/arm64/shared_preferences_foundation-dummy.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/yimisheng/Desktop/AAAucl-2025/T2_1_ucl/casa0021/GPP_May02/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/module.modulemap \
+  /Users/yimisheng/Desktop/AAAucl-2025/T2_1_ucl/casa0021/GPP_May02/macos/Pods/Target\ Support\ Files/shared_preferences_foundation/shared_preferences_foundation-dummy.m \
+  /Users/yimisheng/Desktop/AAAucl-2025/T2_1_ucl/casa0021/GPP_May02/macos/Pods/Target\ Support\ Files/shared_preferences_foundation/shared_preferences_foundation-prefix.pch \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Cocoa.framework/Modules/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
